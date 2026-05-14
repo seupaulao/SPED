291575d6b20d921b59a8699c77cd5671ba22e85b
seupaulao <seupaulao@gmail.com>
2026-05-14 07:31:20 -0300

    movendo backend para pasta especifica

--- src/routes/empresa.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controllers/empresa.controller')
-
-router.get('/', controller.listar)
-router.post('/', controller.criar)
-
-module.exports = router
--- src/routes/relatorio.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controllers/relatorio.controller')
-
-router.get('/balancete/:empresaId', controller.balancete)
-router.get('/dre/:empresaId', controller.dre)
-
-module.exports = router
